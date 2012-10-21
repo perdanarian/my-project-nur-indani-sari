739270b6483b44bc49c42fefe037cf65de5d4db9
indani21 <indani21@b1278a9a-cbd5-1f68-ea4d-d4452f852c0e>
2012-10-21 07:03:42 +0000

    MINGGU 21OKT 2012, PUKUL 14:07 Prak Pbo2 MODIF TEST ANGGOTA

--- trunk/SiPerpus_Danik_New/src/TestPackage/TestAnggota.java
+++ trunk/SiPerpus_Danik_New/src/TestPackage/TestAnggota.java
@@ -1,21 +1,13 @@
 package TestPackage;
 
 import com.model.Anggota;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JOptionPane;
 
 public class TestAnggota {
     public static void main(String[] args)  {
         System.out.println("~~ANGGOTA~~");
         Anggota agg1 = new Anggota();
         agg1.setNama("Denok");
-        try {
-            agg1.setIdAnggota("1234567890");
-        } catch (Exception ex) {
-            JOptionPane.showMessageDialog(null, ex.getMessage());
-            Logger.getLogger(TestAnggota.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        agg1.setIdAnggota("1234567890");
         System.out.println("Nama       : " +agg1.getNama() +
                          "\nId Anggota : " + agg1.getIdAnggota());
     }
