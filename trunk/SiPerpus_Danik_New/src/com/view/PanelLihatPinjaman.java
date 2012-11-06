47215627f9aff9ac3d6508c3c99cdc18000c856f
indani21 <indani21@b1278a9a-cbd5-1f68-ea4d-d4452f852c0e>
2012-11-06 00:43:31 +0000

    6Nov2012 tambah PanelLihatPinjaman pkul 07:43

---
+++ trunk/SiPerpus_Danik_New/src/com/view/PanelLihatPinjaman.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.view;
+
+import com.model.Pinjaman;
+import com.model.PinjamanTableModel;
+import java.awt.BorderLayout;
+import java.util.ArrayList;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+
+/**
+ *
+ * @author Asus
+ */
+public class PanelLihatPinjaman extends JPanel{
+private JTable tablePinjaman;
+private JLabel labelPanel;
+public PanelLihatPinjaman(){}
+private ArrayList<Pinjaman> data;
+
+public PanelLihatPinjaman(ArrayList<Pinjaman> data) {
+this.data = data;
+setLayout(new BorderLayout());
+
+labelPanel = new JLabel("Form Lihat Publikasi");
+labelPanel.setHorizontalAlignment(JLabel.CENTER);
+add(labelPanel,BorderLayout.NORTH);
+
+tablePinjaman = new JTable();
+tablePinjaman.setModel(new PinjamanTableModel(data));
+add(tablePinjaman,BorderLayout.CENTER);
+
+}
+
+public ArrayList<Pinjaman> getData() {
+return data;
+}
+
+public void setData(ArrayList<Pinjaman> data) {
+this.data = data;
+}
+
+}
