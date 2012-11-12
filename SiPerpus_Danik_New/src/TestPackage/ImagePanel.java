9215f08bb81506309a7de50954336a2d7a0fd9f7
indani21 <indani21@b1278a9a-cbd5-1f68-ea4d-d4452f852c0e>
2012-11-12 01:41:26 +0000

    11Nov2012  tambah ImagePanel untuk latihan pukul 8:41

---
+++ SiPerpus_Danik_New/src/TestPackage/ImagePanel.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestPackage;
+
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+import javax.imageio.stream.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author Asus
+ */
+public class ImagePanel extends JPanel {
+
+    private BufferedImage logo;
+
+    /** Creates a new instance of ImagePanel */
+    public ImagePanel() {
+        try {
+// buka file. Cari file gambar lain di computer anda
+            File img = new File("295719_1880934441898_1797445619_1321512_433119823_n.jpg");
+// baca image
+            logo = ImageIO.read(img);
+//img.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+        g.drawImage(logo,
+                90, 90, 480, 360,
+                0, 0, logo.getWidth(null), logo.getHeight(null),
+                null);
+    }
+
+    public static void main(String args[]) {
+        JFrame frame = new JFrame("Using Java2D");
+        frame.setLayout(new BorderLayout());
+        ImagePanel gc = new ImagePanel();
+        frame.add(gc, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 550);
+        frame.setVisible(true);
+    }
+}
