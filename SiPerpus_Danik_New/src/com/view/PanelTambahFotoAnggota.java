835f287b099580fb28eaa0232d7c3a1e6c72c89a
indani21 <indani21@b1278a9a-cbd5-1f68-ea4d-d4452f852c0e>
2012-11-12 03:17:16 +0000

    11Nov2012  edit pukul 10:17

--- SiPerpus_Danik_New/src/com/view/PanelTambahFotoAnggota.java
+++ SiPerpus_Danik_New/src/com/view/PanelTambahFotoAnggota.java
@@ -5,8 +5,20 @@
 
 package com.view;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 /**
@@ -19,33 +31,112 @@ public class PanelTambahFotoAnggota extends JPanel{
     private JTextField textKodeAnggota;
     private JButton tombolSimpan;
     private JButton tombolCariGambar;
-    private File file;
-    private BufferedImage image;
+    private File file= null;
+    private BufferedImage image=null;
 
     public PanelTambahFotoAnggota(){
-    inisialisasi();
+        try {
+            inisialisasi();
+            file = new File("noPicture.png");
+            image = ImageIO.read(file);
+        } catch (IOException ex) {
+            Logger.getLogger(PanelTambahFotoAnggota.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     public void inisialisasi(){
-        setLayout(null);
+        setLayout(new BorderLayout());
+
+
+
+        JPanel panelTombol = new JPanel();
+        panelTombol.setLayout(new FlowLayout());
+
+
         labelKodeAnggota = new JLabel("Kode");
         labelKodeAnggota.setBounds(50, 500, 100, 50);
-        add(labelKodeAnggota);
+        panelTombol.add(labelKodeAnggota);
 
         textKodeAnggota = new JTextField();
+        textKodeAnggota.setColumns(10);
         textKodeAnggota.setBounds(150, 500, 200, 50);
-        add(textKodeAnggota);
+        panelTombol.add(textKodeAnggota);
 
         tombolCariGambar = new JButton("Cari Gambar");
         tombolCariGambar.setBounds(400, 500, 150, 50);
-        add(tombolCariGambar);
+        panelTombol.add(tombolCariGambar);
 
         tombolSimpan = new JButton("Simpan");
         tombolSimpan.setBounds(580, 500, 100, 50);
-        add(tombolSimpan);
+        panelTombol.add(tombolSimpan);
 
+        tombolSimpan.addActionListener(new ActionListener () {
+            public void actionPerformed(ActionEvent e) {
+                tombolSimpanAction();
+            }
+        });
 
+        tombolCariGambar.addActionListener(new ActionListener () {
+            public void actionPerformed(ActionEvent e) {
+                tombolCariGambarAction();
+            }
+        });
 
+        textKodeAnggota.addActionListener(new ActionListener () {
+            public void actionPerformed(ActionEvent e) {
+                textKodeAnggotaAction();
+            }
+        });
+
+        add(panelTombol, BorderLayout.SOUTH);
         setVisible(true);
 
     }
+    public void tombolSimpanAction(){
+        FileInputStream inStream = null;
+        try {
+//            File tujuan = new File(textKodeAnggota.getText() + ".jpg");
+            //        FileInputStream inStream = null;
+            String data = new String();
+//            File inFile = new File(file);
+            inStream = new FileInputStream(file);
+            File tujuan = new File(textKodeAnggota.getText() + ".jpg");
+            int a;
+            while ((a = inStream.read()) != -1) {
+                data = data + (char) a;
+            }
+            inStream.close();
+            System.out.println(data);
+        } catch (IOException ex) {
+            Logger.getLogger(PanelTambahFotoAnggota.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                inStream.close();
+            } catch (IOException ex) {
+                Logger.getLogger(PanelTambahFotoAnggota.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    public void tombolCariGambarAction() {
+        JFileChooser chooser = new JFileChooser();
+        int status = chooser.showOpenDialog(this);
+        int APPROVE_ACTION = 0;
+        if (status==APPROVE_ACTION) {
+            file = chooser.getSelectedFile();
+            repaint();
+        }
+    }
+    public void textKodeAnggotaAction() {
+
+    }
+    public void paintComponents(Graphics g){
+        try {
+            image = ImageIO.read(file);
+            g.drawImage(image,50,50,300,200,0,0,image.getWidth(null),image.getHeight(null),null);
+        } catch (IOException ex) {
+            Logger.getLogger(PanelTambahFotoAnggota.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
 }
