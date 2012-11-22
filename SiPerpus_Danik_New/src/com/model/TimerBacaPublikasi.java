bf3031e1624fa6edf301e71644e3c89360b774f1
indani21 <indani21@b1278a9a-cbd5-1f68-ea4d-d4452f852c0e>
2012-11-22 07:35:29 +0000

    tanggal 22 November 2012 pk. 14:35 TimerBacaPublikasi

---
+++ SiPerpus_Danik_New/src/com/model/TimerBacaPublikasi.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.model;
+
+import java.util.Timer;
+
+/**
+ *
+ * @author Asus
+ */
+public class TimerBacaPublikasi {
+    private int waktuInterval;
+    public TimerBacaPublikasi(int waktuTunda) {
+        this.waktuInterval=waktuInterval;
+        Timer timer = new Timer();
+      //  timer.schedule(new TugasBacaPublikasi(),0,waktuInterval);
+    }
+}
