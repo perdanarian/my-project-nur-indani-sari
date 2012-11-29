5029bc8be47680676cbf4303469ae047ff775cb7
indani21 <indani21@b1278a9a-cbd5-1f68-ea4d-d4452f852c0e>
2012-11-29 17:02:16 +0000

    30 Nov 2012 pk 00:01 tambah kelas untuk timer anggota

---
+++ SiPerpus_Danik_New/src/com/model/TugasSimpanAnggota.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.model;
+
+import java.util.TimerTask;
+
+/**
+ *
+ * @author Asus
+ */
+public class TugasSimpanAnggota extends TimerTask{
+
+    @Override
+    public void run() {
+        Perpustakaan.simpanTabelAnggota();
+    }
+
+}
